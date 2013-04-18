31ce5ea531511b745e3a457646059c110047d291
hobrien <heath.obrien@gmail.com>
2013-04-18 15:00:42 +0100

    Added TopHit.pl which excludes all but the top hit for each query and all but the top query for each hit

---
+++ TopHit.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl -w
+use warnings;
+use strict;
+use Bio::Index::Fasta;
+use Bio::SeqIO;
+use FileParser qw(ParseBlast FlattenBlast);
+$ENV{BIOPERL_INDEX_TYPE} = "SDBM_File";
+
+my $usage = "cat BLASTFILE | TopHit.pl > OUTFILE";
+
+#parse blast hits and make a hash of tops hits (as well as info about multihits)
+my $query_name = 'Init';
+my %top_hits;
+
+while (<>) {
+  chomp;
+  $_ =~ s/,\s*/\t/g;
+  if ( $_ =~ /^#/ or $_ =~ /ADH\d{5}\t/) { next; }
+  my %result = % {ParseBlast($_) };
+  if ( $result{'query_name'} eq $query_name ) { next; } #not the top hit for a contig
+  $query_name = $result{'query_name'};
+  if ( $top_hits{$result{'hit_name'}} ) {               #other query matches same hit
+    my %previous_result = % { ParseBlast($top_hits{$result{'hit_name'}}) };
+    unless ( $result{'score'} > $previous_result{'score'} ) { next; }
+  }
+  $top_hits{$result{'hit_name'}} = FlattenBlast(\%result);
+}  
+
+print "Query_ID	Query_length	Hit_ID	Hit_length	Percent_ID	Aligned_length	Mismatch_count	Gap_count	Query_start	Query_end	Query_frame	Hit_start	Hit_end	Hit_frame	Evalue	Score\n";
+foreach ( keys %top_hits ) {
+  print $top_hits{$_}, "\n";
+}
