a721f9c37797da3595d7eb7b8b8a0398dc832421
hobrien <heath.obrien@gmail.com>
2013-09-06 16:28:19 +0100

    Added a test to see if outfile already exists.
    This allows a batch of jobs to be stopped, then restarted at a later date without having
    to redo the jobs that have already completed

--- RunPhyml.py
+++ RunPhyml.py
@@ -1,7 +1,7 @@
 #!/opt/local/bin/python
 
 import sys
-from os import system
+from os import system, path
 
 def main(argv):
   fasta = argv[0]
@@ -9,10 +9,12 @@ def main(argv):
   phyml_tree = phylip + '_phyml_tree.txt'
   phyml_stats = phylip + '_phyml_stats.txt'
   tree = fasta.replace('.fa', '.nwk')
-  system("mafft --quiet %s |ConvertAln.pl -x fasta -f phyext -o %s -r" % (fasta, phylip))
-  system("phyml --quiet --no_memory_check -i %s" % phylip)
-  system("mv %s %s" % (phyml_tree, tree))
-  system("rm %s" % phyml_stats)
+  if not path.exists(phylip):
+    system("mafft --quiet %s |ConvertAln.pl -x fasta -f phyext -o %s -r" % (fasta, phylip))
+  if not path.exists(tree):
+    system("phyml --quiet --no_memory_check -i %s" % phylip)
+    system("mv %s %s" % (phyml_tree, tree))
+    system("rm %s" % phyml_stats)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
