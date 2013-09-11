cdb8057cb8c7f6586ec0b31271a11b72960f1efc
hobrien <heath.obrien@gmail.com>
2013-09-11 15:58:56 +0100

    bugfix to change os.path to path (which is what was imported)_

--- RunPhyml.py
+++ RunPhyml.py
@@ -9,9 +9,9 @@ def main(argv):
   phyml_tree = phylip + '_phyml_tree.txt'
   phyml_stats = phylip + '_phyml_stats.txt'
   tree = fasta.replace('.fa', '.nwk')
-  if not path.exists(phylip) or os.path.getmtime(fasta) > os.path.getmtime(phylip):
+  if not path.exists(phylip) or path.getmtime(fasta) > path.getmtime(phylip):
     system("mafft --quiet %s |ConvertAln.pl -x fasta -f phyext -o %s -r" % (fasta, phylip))
-  if not path.exists(tree) or os.path.getmtime(fasta) > os.path.getmtime(tree):
+  if not path.exists(tree) or path.getmtime(fasta) > path.getmtime(tree):
     system("phyml --quiet --no_memory_check -i %s" % phylip)
     system("mv %s %s" % (phyml_tree, tree))
     system("rm %s" % phyml_stats)
