84656215c5c06a80b6eb696fd2e1bf0854d6ef5b
hobrien <heath.obrien@gmail.com>
2013-10-08 16:05:35 +0100

    Added a list of clusters corresponding to chloroplast-encoded genes to excluded from analyses
    These are being excluded because many chloroplast genes overlap resulting in chimeric
    transcripts. These transcripts can be extremely long, causing the analysis to really bog down

--- Blast2OrthologGroup.py
+++ Blast2OrthologGroup.py
@@ -1,4 +1,4 @@
-#!/opt/local/bin/python
+#!/usr/local/bin/python
 """Use blast results to add sequences to ortholog groups
 """
 import sys, getopt
@@ -24,8 +24,8 @@ def main(argv):
        blastfilename = arg
     elif opt in ("-s", "--seqfile"):
        seqfilename = arg
-       
-       
+  #Exclude sequences that cluster with chloroplast-encoded genes     
+  chloroplast_clusters = ("Cluster_10175", "Cluster_10226", "Cluster_10232", "Cluster_10485", "Cluster_10984", "Cluster_11190", "Cluster_11826", "Cluster_11977", "Cluster_11980", "Cluster_12324", "Cluster_12326", "Cluster_12658", "Cluster_12697", "Cluster_1272", "Cluster_12984", "Cluster_13661", "Cluster_13666", "Cluster_13836", "Cluster_14312", "Cluster_14375", "Cluster_14565", "Cluster_1826", "Cluster_2867", "Cluster_29556", "Cluster_4023", "Cluster_4421", "Cluster_5258", "Cluster_5739", "Cluster_5972", "Cluster_615", "Cluster_7013", "Cluster_7860", "Cluster_8325", "Cluster_8384")     
   cluster_info = path.join(path.expanduser("~"), "Bioinformatics", "Selaginella", "RefSeq", "SeqClusters.p")
   seq_groups = pickle.load( open( cluster_info, "rb" ) )
   indexfilename = ".".join(seqfilename.split(".")[:-1] + ["inx"])
@@ -35,9 +35,9 @@ def main(argv):
     reader=csv.reader(f,delimiter='\t')
     for row in reader:
       qseqid, qlen, sacc, slen, pident, length, mismatch, gapopen, qstart, qend, qframe, sstart, send, sframe, evalue, bitscore = row
-      if sacc in seq_groups and not used_seqs.has_key(qseqid):
+      if sacc in seq_groups and not used_seqs.has_key(qseqid) and not seq_groups[sacc] in chloroplast_clusters:
         used_seqs[qseqid] = 1
-        seq = new_seqs[qseqid]  
+        seq = new_seqs[qseqid]
         #cluster_filename = path.join(path.expanduser("~"), "Bioinformatics", "Selaginella", "RefSeq", seq_groups[sacc] + ".fa")      
         cluster_filename = path.join(path.expanduser("~"), "Bioinformatics", "Selaginella", "ContigClusters", seq_groups[sacc] + ".fa")     
         cluster_file = open(cluster_filename, "a")
