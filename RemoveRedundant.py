bf67458c5c936716ca5be4077519058e6614cb69
hobrien <heath.obrien@gmail.com>
2013-11-06 11:43:12 +0000

    Rewritten to loop through all tree files in a folder and all 4 Selaginella species
    I've also added an optional GTF file to convert between gene_ids and contig names

--- RemoveRedundant.py
+++ RemoveRedundant.py
@@ -44,52 +44,54 @@ import glob
 
 def main(argv):
   treefolder = ''
-  seqfilename = ''
-  outfilename = ''
   logfilename = ''
-  species_name = ''
   gtf_filename = ''
-  
+  nr_seq_filename = ''
+  usage = 'RemoveRedundant.py -f <folder> -g <gtf_file> -l <logfile> -s <nr_seqs>'
   try:
-    opts, args = getopt.getopt(argv,"ht:n:o:l:s:b:g:",["tree=","infile=","species=", "outfile=","log=", "gtf="])
+    opts, args = getopt.getopt(argv,"hf:l:g:s:",["folder=","log=", "gtf=", "seqs="])
   except getopt.GetoptError:
-    print 'RemoveRedundant.py -t <treefile> -s <sequence_file> -n <species_name> -o <outfile> -l <logfile>'
+    print usage
     sys.exit(2)
   for opt, arg in opts:
     if opt == '-h':
-      print 'RemoveRedundant.py -t <treefile> -s <sequence_file> -n <species_name> -o <outfile> -l <logfile>'
+      print usage
       sys.exit()
-    elif opt in ("-t", "--tree"):
+    elif opt in ("-f", "--folder"):
       treefolder = arg
-    elif opt in ("-n", "--name"):
-      species_name = arg
-    elif opt in ("-o", "--outfile"):
-      outfilename = arg
     elif opt in ("-l", "--log"):
       logfilename = arg
-    elif opt in ("-s", "--sequences"):
-      seqfilename = arg
     elif opt in ("-g", "--gtf"):
       gtf_filename = arg
+    elif opt in ("-s", "--seqs"):
+      nr_seq_filename = arg
 
-  #Build database of sequences (if one doesn't already exist)   
-  indexfilename = seqfilename + ".inx"
-  seq_db = SeqIO.index_db(indexfilename, seqfilename, "fasta")
-  
   #make dictionary to translate gene_ids back to contig names (this might be slow)
+  #This will need to be a concatinated file if I'm going to loop over species
   if gtf_filename:
      gene_ids = get_id_dict(gtf_filename)
 
+  #make dictionary of nr seqs (this is probably a temp
+  if nr_seq_filename:
+    nr_seqs = {}
+    nr_seq_fh = open(nr_seq_filename, 'r')
+    if gtf_filename:
+      for seq in nr_seq_fh.readlines():
+        nr_seqs[gene_ids[seq.rstrip()]] = 1
+    else:
+      for seq in nr_seq_fh.readlines():
+        nr_seqs[seq] = 1
+      
+    nr_seq_fh.close()
 
   con = mdb.connect('localhost', 'root', '', 'Selaginella');
   with con:
-    outfile = open(outfilename, "w")
     logfile = open(logfilename, "w")
     cur = con.cursor()
     seq_total = 0
     for treefile in glob.glob(path.join(treefolder, '*')):
       if '.nwk' not in treefile:
-        continue 
+        continue
       #read in tree and root by midpoint
       try:
         t = Tree(treefile)
@@ -106,37 +108,50 @@ def main(argv):
       #parse tree file to get cluster number
       cluster_num = path.split(treefile)[1].split(".")[0] #get cluster number
 
+      #open corresponding fasta file and read sequences into dictionary
+      seqfilename = treefile.replace("nwk", "fa")
+      print seqfilename
+      seqfilehandle = open(seqfilename, 'r')
+      seq_dict = SeqIO.to_dict(SeqIO.parse(seqfilehandle, "fasta"))
+      seqfilehandle.close()
+      for key in seq_dict.keys():
+        print key
+
       #cycle through monophyletic groups and select sequence with longest blast hit for each group
-      for node in t.get_monophyletic(values=[species_name], target_attr="species"):
-        species_seqs = []
-        for leaf in node:
-          if 'kraussiana' not in leaf.name and 'willdenowii' not in leaf.name:  #exclude reference sequence from seq list
-            if gtf_filename:
-              try:
-                species_seqs.append(gene_ids[leaf.name])
-              except KeyError:
-                print "%s in %s not found in GTF file" % (leaf.name, treefile)
-            else:
+      for species_name in ('KRAUS', 'MOEL', 'UNC', 'WILD'):
+        for node in t.get_monophyletic(values=[species_name], target_attr="species"):
+          species_seqs = []
+          for leaf in node:
+            if 'kraussiana' not in leaf.name and 'willdenowii' not in leaf.name:  #exclude reference sequence from seq list
               species_seqs.append(leaf.name)
-            seq_total += 1
-        rep_seq = ''
-        for seq in species_seqs:
-          if rep_seq and get_length(cur, rep_seq) >= get_length(cur, seq): #saved rep seq longer than current seq
-            pass
-          else:                       #current seq longer (or no saved seq)
-            rep_seq = seq
-        if rep_seq:
-          try:
-            outfile.write(seq_db.get_raw(rep_seq))
-          except KeyError:
-            sys.exit("No sequence %s in %s" % ( rep_seq, seqfilename))
+              seq_total += 1
+          rep_seq = ''
+          if nr_seq_filename:
+            for seq in species_seqs:
+              if seq in nr_seqs:
+                rep_seq = seq
+              else:
+                del seq_dict[seq]
+          else:
+            for seq in species_seqs:
+              if gtf_filename:
+                try:
+                  seq_name = gene_ids[seq]
+                except KeyError:
+                  sys.exit("%s in %s not found in GTF file" % (seq, treefile))
+              else:
+                seq_name = seq
+              if rep_seq and get_length(cur, rep_seq) >= get_length(cur, seq_name): #saved rep seq longer than current seq
+                del seq_dict[seq]
+              else:                       #current seq longer (or no saved seq)
+                if rep_seq:
+                  del seq_dict[rep_seq]
+                rep_seq = seq
           for seq in species_seqs:
             logfile.write("%s\n" % ", ".join([seq, rep_seq, cluster_num]))
-
-
-    print seq_total
+      write_seqs(seq_dict, seqfilename, "fasta")
+      print seq_total
     logfile.close()
-    outfile.close()
 
 def get_length(c, id):
   try:
@@ -165,9 +180,15 @@ def get_id_dict(filename):
   for line in open(filename, 'r').readlines():
     feature = parse_GTF(line.split("\t"))
     if 'gene_id' in feature.keys() and feature['gene_id'] != 'NULL':
-        seqids[feature['gene_id']] = feature['seqid']
+        seqids[feature['seqid']] = feature['gene_id']
   return seqids
 
+def write_seqs(dict, file, format):
+  fh = open(file, 'w')
+  for seq in dict.values():
+    fh.write(seq.format(format))
+  fh.close()
+  
 if __name__ == "__main__":
    main(sys.argv[1:])
    #import timeit
