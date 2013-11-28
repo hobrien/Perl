e2c536ad4176d28e05639de611c7ff5bb0f2f731
hobrien <heath.obrien@gmail.com>
2013-11-28 14:17:02 +0000

    Reorganized so that adding info from TAIR is a done by a function that is specified with the TAIR option

--- AddData.py
+++ AddData.py
@@ -1,45 +1,58 @@
 #!/usr/local/bin/python
 
+"""
+A number of functions to add data to Selaginella database from csv files or from sequence
+files"""
+
+
 import sys, getopt, csv
 import MySQLdb as mdb
 from os import path
 
 
 def main(argv):
+  usage = 'AddData -f <function> -i <inputfile>
   infilename = ''
+  function = ''
   try:
-     opts, args = getopt.getopt(argv,"hi:",["ifile="])
+     opts, args = getopt.getopt(argv,"hf:i:",["function", "ifile="])
   except getopt.GetoptError:
-     print 'AddOrthologs.py -i <inputfile>'
+     print usage
      sys.exit(2)
   for opt, arg in opts:
      if opt == '-h':
-        print 'AddOrthologs.py -i <inputfile>'
+        print usage
         sys.exit()
      elif opt in ("-i", "--ifile"):
         infilename = arg
+    elif opt in ("-f", "--function"):
+        function? = arg
 
   con = mdb.connect('localhost', 'root', '', 'Selaginella');
   with con:
     cur = con.cursor()
-    infile = open(infilename, 'r')
-    for line in infile.readlines():
-      line = line.strip()
-      fields = line.split('\t')
-      locus = fields[0]
-      if locus == 'Locus Identifier': #header row
-        continue
-      subfields = fields[2].split(';')
-      description = subfields[0]
-      try:
-        symbol = fields[4]
-      except IndexError:
-        symbol = ''
-      print "INSERT INTO tair (locus, description, symbol) VALUES ('%s', '%s', '%s')" % (locus, description, symbol)
-      try:
-        cur.execute("INSERT INTO tair (locus, description, symbol) VALUES (%s, %s, %s)", (locus, description, symbol))
-      except mdb.IntegrityError, e:
-        continue
+    if function == 'TAIR':
+      AddTAIR(cur, infilename)
+      
+def addTAIR(cur, infilename):
+  infile = open(infilename, 'r')
+  for line in infile.readlines():
+    line = line.strip()
+    fields = line.split('\t')
+    locus = fields[0]
+    if locus == 'Locus Identifier': #header row
+      continue
+    subfields = fields[2].split(';')
+    description = subfields[0]
+    try:
+      symbol = fields[4]
+    except IndexError:
+      symbol = ''
+    print "INSERT INTO tair (locus, description, symbol) VALUES ('%s', '%s', '%s')" % (locus, description, symbol)
+    try:
+      cur.execute("INSERT INTO tair (locus, description, symbol) VALUES (%s, %s, %s)", (locus, description, symbol))
+    except mdb.IntegrityError, e:
+      continue
 
 if __name__ == "__main__":
    main(sys.argv[1:])
