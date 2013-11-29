3193508878d7cf022b9789225e68877d09757c5a
hobrien <heath.obrien@gmail.com>
2013-11-29 13:23:51 +0000

    Reorganised so that getting different info is done by calling functions
    currently, the only kind of info that can be retrieved is TAIR info

--- GetData.py
+++ GetData.py
@@ -7,34 +7,41 @@ from os import path
 
 def main(argv):
   infilename = ''
+  function = ''
+  usage = "GetData.py -f <function> -i <filename>"
   try:
-     opts, args = getopt.getopt(argv,"hi:",["ifile="])
+     opts, args = getopt.getopt(argv,"hf:i:",["function", "ifile="])
   except getopt.GetoptError:
-     print 'AddOrthologs.py -i <inputfile>'
+     print usage
      sys.exit(2)
   for opt, arg in opts:
      if opt == '-h':
-        print 'AddOrthologs.py -i <inputfile>'
+        print usage
         sys.exit()
      elif opt in ("-i", "--ifile"):
         infilename = arg
+    elif opt in ("-f", "--function"):
+        function? = arg
 
   con = mdb.connect('localhost', 'root', '', 'Selaginella');
   with con:
     cur = con.cursor()
-    infile = open(infilename, 'r')
-    for line in infile.readlines():
-      line = line.strip()
-      fields = line.split(' ')
-      cluster = fields[0]
-      if cluster == 'Locus Identifier': #header row
-        continue
-      cluster = cluster.replace('cluster_', '')
-      #print "SELECT seqid FROM sequences WHERE species = 'ATH' AND clusternum = %s" % (cluster)
-      cur.execute("SELECT sequences.clusternum, sequences.seqid, tair.description, tair.symbol FROM sequences, tair WHERE sequences.seqid = tair.locus AND species = 'ATH' AND clusternum = %s", (cluster))
-      for row in cur.fetchall():
-        print '\t'.join(map(str, row))
-      #cur.execute("INSERT INTO tair (locus, description, symbol) VALUES (%s, %s, %s)", (locus, description, symbol))
+    if function == 'TAIR':
+      get_TAIR(cur, infilename)
+      
+def get_TAIR:
+  infile = open(infilename, 'r')
+  for line in infile.readlines():
+    line = line.strip()
+    fields = line.split(' ')
+    cluster = fields[0]
+    if cluster == 'Locus Identifier': #header row
+      continue
+    cluster = cluster.replace('cluster_', '')
+    #print "SELECT seqid FROM sequences WHERE species = 'ATH' AND clusternum = %s" % (cluster)
+    cur.execute("SELECT sequences.clusternum, sequences.seqid, tair.description, tair.symbol FROM sequences, tair WHERE sequences.seqid = tair.locus AND species = 'ATH' AND clusternum = %s", (cluster))
+    for row in cur.fetchall():
+      print '\t'.join(map(str, row))
   
 
 if __name__ == "__main__":
