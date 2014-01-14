a50a21511157c807a4b07fb1d9a759265a0d9c31
hobrien <heath.obrien@gmail.com>
2014-01-14 14:25:26 +0000

    modified test_get_taxid to test for GI numbers as input

--- test/test_BlastTools.py
+++ test/test_BlastTools.py
@@ -3,7 +3,9 @@ from BlastTools import get_taxid
 P_venosa_id = 162011
    
 def test_get_taxid():
-  assert int(get_taxid('KC437649')) == P_venosa_id
-  assert int(get_taxid('KC437649.1')) == P_venosa_id
-  assert int(get_taxid('gi|455475748|gb|KC437649.1|')) == P_venosa_id 
-  assert int(get_taxid('gi|455475748|gb|KC437649.1')) == P_venosa_id 
+  assert get_taxid('gi|455475748|gb|KC437649.1|') == P_venosa_id 
+  assert get_taxid('gi|455475748|gb|KC437649.1') == P_venosa_id 
+  assert get_taxid('KC437649') == P_venosa_id
+  assert get_taxid('KC437649.1') == P_venosa_id
+  assert get_taxid('gi|455475748') == P_venosa_id 
+  assert get_taxid('455475748') == P_venosa_id 
