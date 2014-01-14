7c9e02f1e2116569df6b4aac40fff807e076a5bf
hobrien <heath.obrien@gmail.com>
2014-01-14 14:24:01 +0000

    modified get_taxid to return an integer rather than a string

--- Modules/BlastTools.py
+++ Modules/BlastTools.py
@@ -3,11 +3,12 @@
 import subprocess
 
 def get_taxid(id):
-  #id = "'%s'" % id
+  """takes GI number, Genbank accession number or full genbank id and looks up taxID number
+  using blastdbcmd"""
   options = ("blastdbcmd -entry '" + id + "' -db nt -outfmt %T")
   proc = subprocess.Popen(options, stdout=subprocess.PIPE, shell=True)
   staxids = proc.communicate()
-  return staxids[0].strip()
+  return int(staxids[0].strip())
   
 
 
