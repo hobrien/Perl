dc065570bf074090401bb60fada6751806239510
hobrien <heath.obrien@gmail.com>
2014-01-20 18:25:42 +0000

    first commit (combine_clusters function)

---
+++ Modules/SQLTools.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python
+
+import MySQLdb as mdb
+
+def combine_clusters(cluster1, cluster2, db='Selaginella'):
+  con = mdb.connect('localhost', 'root', '', db)
+  with con:
+    cur = con.cursor()
+    cur.execute("SELECT seqid FROM cluster_num WHERE cluster = %s", cluster1)
+    for seq in cur.fetchall():
+      cur.execute("SELECT seqid FROM cluster_num WHERE cluster = %s AND seqid = %s", (cluster2, seq[0]))
+      if len(cur.fetchall()) > 0:
+        #print "DELETE FROM cluster_num WHERE seqid = %s AND cluster = %s" % (seq[0], cluster1)
+        cur.execute("DELETE FROM cluster_num WHERE seqid = %s AND cluster = %s", (seq[0], cluster1))
+      else:
+        cur.execute("UPDATE cluster_num SET cluster = %s WHERE seqid = %s", (cluster2, seq[0]))
---
+++ test/test_SQLTools.py
@@ -0,0 +1,35 @@
+from SQLTools import combine_clusters
+import MySQLdb as mdb
+from nose.tools import *
+
+def test_combine_clusters():
+  con = mdb.connect('localhost', 'root', '', 'test')
+  with con:
+    cur = con.cursor()
+    cur.execute("""DROP TABLE IF EXISTS `cluster_num`;""")
+    
+    cur.execute("""CREATE TABLE `cluster_num` (
+                     `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+                     `seqid` char(25) DEFAULT NULL,
+                     `cluster` int(11) unsigned NOT NULL,
+                     PRIMARY KEY (`id`),
+                     KEY `seqid_inx` (`seqid`),
+                     KEY `cluster_inx` (`cluster`)
+                   ) ENGINE=InnoDB DEFAULT CHARSET=latin1;"""
+                )
+
+    cur.execute("""INSERT INTO `cluster_num` (`id`, `seqid`, `cluster`)
+                     VALUES
+                	   (2,'Seq1',2),
+                	   (4,'Seq2',2),
+                	   (5,'Seq3',2),
+                	   (6,'Seq4',2);"""
+                )
+
+  combine_clusters(2,1,'test')
+  con = mdb.connect('localhost', 'root', '', 'test')
+  with con:
+    cur.execute("SELECT seqid, cluster FROM cluster_num")
+    results = cur.fetchall()
+    print results
+    assert results == (('Seq1', 1), ('Seq2', 1), ('Seq3', 1),('Seq4', 1))
