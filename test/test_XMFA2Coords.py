ddc1ed9247c49fa8b9f15499c06fa6228605f88c
hobrien <heath.obrien@gmail.com>
2014-05-12 12:43:06 +0100

    Added basic tests for the main loop (parse_coords)

--- test/test_XMFA2Coords.py
+++ test/test_XMFA2Coords.py
@@ -1,5 +1,9 @@
-from XMFA2Coords import parse_id
+from XMFA2Coords import parse_id, parse_coords
 import tempfile, os, warnings
+from Bio.Alphabet import generic_dna, IUPAC
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.Align import MultipleSeqAlignment
 from nose.tools import *
 from nose import with_setup
 
@@ -24,3 +28,25 @@ def test_parse_id2():
 def test_parse_id3():
   """test if error handling works correctly when strand info not formatted correctly"""
   (start, strand, name) = parse_id('1:1-20 1 /Users/HeathOBrien/Bioinformatics/Psuedomonas/AE016853.gbk')
+
+def test_XMFA2Coords():
+    """test if coordinate parsing works correctly"""
+    align1 = MultipleSeqAlignment([
+             SeqRecord(Seq("ACTGCTAGCTAG", generic_dna), id="1:1-20 + Seq1"),
+             SeqRecord(Seq("ACT-CTAGCCAG", generic_dna), id="2:2-21 + Seq2"),
+             SeqRecord(Seq("ACTGCTAGCTAG", generic_dna), id="1:1-20 - Seq3"),
+         ])
+    coords = parse_coords(align1)
+    print len(coords)
+    assert len(coords) == 12
+    print len(coords[0])
+    assert len(coords[0]) == 3
+    print coords[0][0]
+    assert coords[0][0] == "Seq1:1"
+    print coords[3][1]
+    assert coords[3][1] == "-"
+    print coords[4][1]
+    assert coords[4][1] == "Seq2:5"
+    print coords[-1][2]
+    assert coords[-1][2] == "Seq3:9"
+    
