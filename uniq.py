72387d9585f97094e3f24e4f756a8fe6cbfb828f
hobrien <heath.obrien@gmail.com>
2014-05-20 11:25:31 +0100

    fixed bug in argument parser so that long argument names work correctly

--- uniq.py
+++ uniq.py
@@ -6,9 +6,9 @@ import argparse, sys
 parser = argparse.ArgumentParser(description='like unix uniq, but allows specification of a specific field and need not be sorted')
 parser.add_argument('files', metavar='files', type=argparse.FileType('r'), nargs='?',
                    default=sys.stdin, help='list of files to process')
-parser.add_argument('-k, --column', dest='column', default=1, type=int,
+parser.add_argument('-k', '--column', dest='column', default=1, type=int,
                    help='column used for comparison of unique')
-parser.add_argument('-d, --delimitor', dest='sep', default='', type=str,
+parser.add_argument('-d', '--delimitor', dest='sep', default='', type=str,
                    help='column used for comparison of unique')
 
 args = parser.parse_args()
